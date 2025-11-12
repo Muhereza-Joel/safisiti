bc8aa9096e02cbc0d096ca4e701f7348df1cabfd
Muhereza-Joel <muherezajoel40@gmail.com>
2025-11-12 13:46:30 +0300

    update command for sync id/uuid

--- app/Filament/Resources/CollectionPointResource/Pages/ListCollectionPoints.php
+++ app/Filament/Resources/CollectionPointResource/Pages/ListCollectionPoints.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\CollectionPointResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Components\Tab;
+use Illuminate\Support\Facades\Artisan;
+use Filament\Notifications\Notification;
 
 class ListCollectionPoints extends ListRecords
 {
@@ -15,6 +17,28 @@ class ListCollectionPoints extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+
+            // --- ADD THIS ACTION ---
+            Actions\Action::make('backfill_uuids')
+                ->label('Sync ID/UUIDs')
+                ->icon('heroicon-o-arrow-path')
+                ->color('warning') // Use a color to show it's a special action
+                ->requiresConfirmation() // Ask the user "Are you sure?"
+                ->modalHeading('Sync Collection Point IDs/UUIDs')
+                ->modalDescription('Are you sure you want to run the ID/UUID sync? This will scan the database and fix any missing links between collection points, wards and cells. This is safe to run anytime.')
+                ->modalSubmitActionLabel('Yes, run sync')
+                ->action(function () {
+                    // 1. Call your Artisan command
+                    Artisan::call('backfill:collection-point-uuids');
+
+                    // 2. Send a success notification
+                    Notification::make()
+                        ->title('Sync Complete')
+                        ->body('The Collection Point ID/UUID sync has been successfully executed.')
+                        ->success()
+                        ->send();
+                }),
+            // --- END OF ACTION ---
         ];
     }
 
