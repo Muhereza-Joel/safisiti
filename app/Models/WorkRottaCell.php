5c5f9ccf1533bb41598262b64f60c5c404960d47
Muhereza-Joel <muherezajoel40@gmail.com>
2025-11-13 06:22:45 +0300

    bug fix

--- app/Models/WorkRottaCell.php
+++ app/Models/WorkRottaCell.php
@@ -16,9 +16,7 @@ class WorkRottaCell extends Model
 
     protected $fillable = [
         'uuid',
-        'work_rotta_id',
         'work_rotta_uuid',
-        'cell_id',
         'cell_uuid',
         'status',
         'organisation_id',
@@ -60,20 +58,6 @@ class WorkRottaCell extends Model
     {
         // Before save (create or update)
         static::saving(function ($model) {
-            // Sync Work Rotta ID/UUID
-            if ($model->isDirty('work_rotta_id') && !$model->isDirty('work_rotta_uuid')) {
-                $model->work_rotta_uuid = $model->workRotta?->uuid;
-            } elseif ($model->isDirty('work_rotta_uuid') && !$model->isDirty('work_rotta_id')) {
-                $model->work_rotta_id = $model->workRotta?->id;
-            }
-
-            // Sync Cell ID/UUID
-            if ($model->isDirty('cell_id') && !$model->isDirty('cell_uuid')) {
-                $model->cell_uuid = $model->cell?->uuid;
-            } elseif ($model->isDirty('cell_uuid') && !$model->isDirty('cell_id')) {
-                $model->cell_id = $model->cell?->id;
-            }
-
             // Sync Organisation ID/UUID
             if ($model->isDirty('organisation_id') && !$model->isDirty('organisation_uuid')) {
                 $model->organisation_uuid = $model->organisation?->uuid;
@@ -100,23 +84,6 @@ class WorkRottaCell extends Model
                 }
             }
 
-            // Backfill missing UUIDs or IDs
-            if (!empty($model->cell_id) && empty($model->cell_uuid)) {
-                $model->cell_uuid = \App\Models\Cell::find($model->cell_id)?->uuid;
-            }
-
-            if (!empty($model->work_rotta_id) && empty($model->work_rotta_uuid)) {
-                $model->work_rotta_uuid = \App\Models\WorkRotta::find($model->work_rotta_id)?->uuid;
-            }
-
-            // Sync back IDs if only UUIDs provided
-            if (!empty($model->cell_uuid) && empty($model->cell_id)) {
-                $model->cell_id = \App\Models\Cell::where('uuid', $model->cell_uuid)->value('id');
-            }
-
-            if (!empty($model->work_rotta_uuid) && empty($model->work_rotta_id)) {
-                $model->work_rotta_id = \App\Models\WorkRotta::where('uuid', $model->work_rotta_uuid)->value('id');
-            }
 
             if (!empty($model->organisation_uuid) && empty($model->organisation_id)) {
                 $model->organisation_id = \App\Models\Organisation::where('uuid', $model->organisation_uuid)->value('id');
